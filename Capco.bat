d64fc744d766cfee8f91bc2634f3469dd5a502c2
Lokesh Bandi <lokeshbandireddy@gmail.com>
2021-07-27 18:02:30 +0530

    Update Capco.bat

--- Capco.bat
+++ Capco.bat
@@ -3,4 +3,4 @@ echo JAVA_HOME is %JAVA_HOME%
 echo SCALA_HOME is %SCALA_HOME%
 set arg1=%1
 echo ****Answer****
-scala -cp ./target/sudoku-1.0-SNAPSHOT.jar com.test.App %arg1%
+scala -cp ./sudoku-1.0-SNAPSHOT.jar com.test.App %arg1%
