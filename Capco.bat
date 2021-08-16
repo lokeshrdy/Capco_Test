604887760c9448335963003b1ee268fcff77f61b
Lokesh Bandi <lokeshbandireddy@gmail.com>
2021-08-17 00:30:35 +0530

    Add files via upload

--- Capco.bat
+++ Capco.bat
@@ -2,4 +2,4 @@
 echo JAVA_HOME is %JAVA_HOME%
 set arg1=%1
 echo ****Answer****
-java -cp ./sudoku-1.0-SNAPSHOT.jar com.test.Sudoku %arg1%
+java -cp ./sudoku-1.0-SNAPSHOT.jar com.capco.test.Sudoku %arg1%
